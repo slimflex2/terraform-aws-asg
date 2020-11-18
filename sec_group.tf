545b12e3e1766060b1a32e6d8163ca4885ea8f94
bernard <slimflex2@hotmail.com>
2020-11-18 01:45:20 +0000

    added changes

--- sec_group.tf
+++ sec_group.tf
@@ -9,13 +9,14 @@ resource "aws_security_group" "asg-sec-group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- ingress {
+  ingress {
     description = "TLS from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
